524e009d44acfdea64a01a0a2d6237a3b3457458
Ayoub Eddaoudi <me@ayoubedd.me>
2024-07-01 01:22:40 +0100

    fix(homes/orbit): fix git aliases and move git config to its own module

--- homes/orbit/default.nix
+++ homes/orbit/default.nix
@@ -12,6 +12,8 @@
     ../../modules/home-manager/editors/neovim
     ../../modules/home-manager/terminals/alacritty
     ../../modules/home-manager/shells/zsh
+
+    ./git.nix
   ];
 
   home.sessionVariables = import ./vars.nix;
@@ -20,68 +22,7 @@
   home.homeDirectory = "/home/orbit";
 
   programs.home-manager.enable = true;
-  programs.git = {
-    enable = true;
-    userEmail = "me@ayoubedd.me";
-    userName = "Ayoub Eddaoudi";
-    includes = [
-      {
-        condition = "gitdir:~/Code/42/";
-        contents = {
-          user = {
-            email = "aeddaoud@student.1337.ma";
-            name = "Ayoub Eddaoudi";
-          };
-        };
-      }
-    ];
-    extraConfig = {
-      core.excludesfile = "gitignore";
-      core.editor = "nvim";
-      core.pager = "delta";
-      branch.sort = "-committerdate";
-      color.ui = "auto";
-      init.defaultBranch = "master";
-      aliases = {
-        # add
-        a = "add";
-        aa = "add .";
 
-        # commit
-        c = "commit";
-        cm = "commit -m";
-        cam = "commit -am";
-
-        # status
-        s = "status -s";
-
-        # push
-        p = "push";
-        pf = "push --force-with-lease";
-
-        # pull
-        pl = "pull";
-
-        # clone
-        cl = "clone";
-        cls = "clone --depth=1"; # shallow clone
-
-        # diff
-        d = "diff";
-        ds = "diff --staged";
-
-        # log
-        l = "log";
-        lo = ''log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short'';
-
-        # stash
-        staash = "stash --all";
-
-        # branch
-        b = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
-      };
-    };
-  };
   home.stateVersion = "24.11";
 }
 
---
+++ homes/orbit/git.nix
@@ -0,0 +1,64 @@
+{ ... }: {
+  programs.git = {
+    enable = true;
+    userEmail = "me@ayoubedd.me";
+    userName = "Ayoub Eddaoudi";
+    includes = [
+      {
+        condition = "gitdir:~/Code/42/";
+        contents = {
+          user = {
+            email = "aeddaoud@student.1337.ma";
+            name = "Ayoub Eddaoudi";
+          };
+        };
+      }
+    ];
+    extraConfig = {
+      core.excludesfile = "gitignore";
+      core.editor = "nvim";
+      core.pager = "delta";
+      branch.sort = "-committerdate";
+      color.ui = "auto";
+      init.defaultBranch = "master";
+      alias = {
+        # add
+        a = "add";
+        aa = "add .";
+
+        # commit
+        c = "commit";
+        cm = "commit -m";
+        cam = "commit -am";
+
+        # status
+        s = "status -s";
+
+        # push
+        p = "push";
+        pf = "push --force-with-lease";
+
+        # pull
+        pl = "pull";
+
+        # clone
+        cl = "clone";
+        cls = "clone --depth=1"; # shallow clone
+
+        # diff
+        d = "diff";
+        ds = "diff --staged";
+
+        # log
+        l = "log";
+        lo = ''log --pretty=format:"%C(yellow)%h\\ %ad%Cred%d\\ %Creset%s%Cblue\\ [%cn]" --decorate --date=short'';
+
+        # stash
+        staash = "stash --all";
+
+        # branch
+        b = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
+      };
+    };
+  };
+}
