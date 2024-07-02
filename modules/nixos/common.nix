cdeaef6729d48134651a28dd1cb63c17122b6a5e
Ayoub Eddaoudi <me@ayoubedd.me>
2024-07-02 07:12:33 +0100

    chore(modules/nixos): add Cachyos udev rules

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -17,5 +17,18 @@
     package = pkgs.ananicy-cpp;
     rulesProvider = pkgs.ananicy-rules-cachyos;
   };
+
+  services.udev.extraRules = ''
+    # HDD
+    ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+
+    # SSD
+    ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+
+    # NVMe SSD
+    ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
+
+    DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
+  '';
 }
 
