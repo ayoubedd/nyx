294225a49708116b57c9180cad9e9ea60e80021c
Ayoub Eddaoudi <me@ayoubedd.me>
2024-07-09 14:40:59 +0100

    chore(mixins/nixos): enable store optimizations and auto garbage collection

--- mixins/nixos/nix.nix
+++ mixins/nixos/nix.nix
@@ -1,3 +1,9 @@
 { ... }: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.optimise.automatic = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
 }
