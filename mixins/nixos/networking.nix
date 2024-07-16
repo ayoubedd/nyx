5c83f4fdab2b077d509a0397f185f3bdf50bbe9a
Ayoub Eddaoudi <me@ayoubedd.me>
2024-07-16 14:35:15 +0100

    chore(mixins/nixos): disable DNSSEC due to the horrible performance

--- mixins/nixos/networking.nix
+++ mixins/nixos/networking.nix
@@ -6,18 +6,18 @@
   # networking.firewall.allowedTCPPorts = [ ];
   # networking.firewall.allowedUDPPorts = [ ];
 
-  networking.nameservers = [
-    "1.1.1.1#one.one.one.one"
-    "1.0.0.1#one.one.one.one"
-    "8.8.8.8#dns.google"
-    "8.4.4.8#dns.google"
-  ];
+  # networking.nameservers = [
+  #   "1.1.1.1#one.one.one.one"
+  #   "1.0.0.1#one.one.one.one"
+  #   "8.8.8.8#dns.google"
+  #   "8.4.4.8#dns.google"
+  # ];
 
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    dnsovertls = "true";
-    domains = [ "~." ];
+    # dnssec = "true";
+    # dnsovertls = "true";
+    # domains = [ "~." ];
     fallbackDns = [
       "1.1.1.1#one.one.one.one"
       "1.0.0.1#one.one.one.one"
