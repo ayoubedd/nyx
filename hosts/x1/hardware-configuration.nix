6e62c0809d7632044797aec51eecc13c7a3f398e
Ayoub Eddaoudi <me@ayoubedd.me>
2024-10-10 21:38:26 +0100

    feat(hosts/x1): remove intel-vaapi-driver driver

--- hosts/x1/hardware-configuration.nix
+++ hosts/x1/hardware-configuration.nix
@@ -34,7 +34,7 @@
     enable32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      # intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
       libvdpau-va-gl
     ];
   };
