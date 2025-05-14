a353e121e09c9fec908db24b44536dbe9587d03c
Ayoub Eddaoudi <me@ayoubedd.me>
2025-05-14 02:39:34 +0100

    feat(x1): add wireplumber config to disable uneeded sinks and inputs & rename others

--- machines/x1/misc.nix
+++ machines/x1/misc.nix
@@ -39,4 +39,46 @@
     # NVMe SSD
     ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
   '';
+
+  services.pipewire.wireplumber.extraConfig = {
+    "1000-all" = {
+      "monitor.alsa.rules" = [
+        {
+          matches = [
+            {
+              "node.name" =
+                "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI1__sink";
+            }
+            {
+              "node.name" =
+                "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI2__sink";
+            }
+            {
+              "node.name" =
+                "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__HDMI3__sink";
+            }
+            {
+              "node.name" =
+                "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic2__source";
+            }
+          ];
+          actions = { update-props = { "node.disabled" = true; }; };
+        }
+        {
+          matches = [{
+            "node.name" =
+              "alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source";
+          }];
+          actions = { update-props = { "node.description" = "Laptop Mic"; }; };
+        }
+        {
+          matches = [{
+            "node.name" =
+              "alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Speaker__sink";
+          }];
+          actions = { update-props = { "node.description" = "Laptop"; }; };
+        }
+      ];
+    };
+  };
 }
