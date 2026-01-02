42499100f5fa72efe9b54538b3fa5c08eb994314
Ayoub Eddaoudi <me@ayoubedd.me>
2026-01-02 21:25:32 +0100

    feat(desktops/hyprland/nvim): update plugins / adapt to new project.nvim api

--- misc/hm/desktops/hyprland/nvim/conf/lua/plugins/project.lua
+++ misc/hm/desktops/hyprland/nvim/conf/lua/plugins/project.lua
@@ -1,7 +1,7 @@
 return {
 	"DrKJeff16/project.nvim", -- Changes PWD to the root of the project
 	config = function()
-		vim.g.project_lsp_nowarn = 1
+		-- vim.g.project_lsp_nowarn = 1
 		require("project").setup({
 			-- Manual mode doesn't automatically change your root directory, so you have
 			-- the option to manually do so using `:ProjectRoot` command.
@@ -11,11 +11,28 @@ return {
 			-- lsp, while **"pattern"** uses vim-rooter like glob pattern matching. Here
 			-- order matters: if one is not detected, the other is used as fallback. You
 			-- can also delete or rearangne the detection methods.
-			detection_methods = { "lsp", "pattern" },
+			use_lsp = true,
 
 			-- All the patterns used to detect root dir, when **"pattern"** is in
 			-- detection_methods
-			patterns = { ".git", "_darcs", "flake.nix", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+			patterns = {
+				"_darcs",
+				"flake.nix",
+				".hg",
+				".bzr",
+				"Makefile",
+				"package.json",
+				".git",
+				".github",
+				".svn",
+				"Pipfile",
+				"pyproject.toml",
+				".pre-commit-config.yaml",
+				".pre-commit-config.yml",
+				".csproj",
+				".sln",
+				".nvim.lua",
+			},
 
 			-- Table of lsp clients to ignore by name
 			-- eg: { "efm", ... }
