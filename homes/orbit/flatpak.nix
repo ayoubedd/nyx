19fb0085c10a814d2195b2560c3c8738e81d1ee2
Ayoub Eddaoudi <me@ayoubedd.me>
2026-01-22 04:36:29 +0100

    feat(homes/orbit): remove a few unused flatpaks

--- homes/orbit/flatpak.nix
+++ homes/orbit/flatpak.nix
@@ -5,30 +5,11 @@
   ];
 
   services.flatpak.packages = [
-    "com.obsproject.Studio"
     "com.brave.Browser"
-    "com.spotify.Client"
-    "dev.vencord.Vesktop"
     "com.github.tchx84.Flatseal"
-    "com.getpostman.Postman"
     "org.inkscape.Inkscape"
     "org.gimp.GIMP"
   ];
 
   services.flatpak.uninstallUnmanaged = true;
-
-  services.flatpak.overrides = {
-    "com.getpostman.Postman".Context.sockets = [
-      "wayland"
-      "!x11"
-      "!fallback-x11"
-    ];
-
-    "dev.vencord.Vesktop".Context.sockets = [
-      "wayland"
-      "!x11"
-      "!fallback-x11"
-    ];
-
-  };
 }
