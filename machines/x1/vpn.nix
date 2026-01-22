a78d12645db276ddefd1aa7135ba6c62f9ab222d
Ayoub Eddaoudi <me@ayoubedd.me>
2026-01-22 04:07:52 +0100

    feat(machines/x1): switch to wg-quick for wireguard

--- machines/x1/vpn.nix
+++ machines/x1/vpn.nix
@@ -2,9 +2,11 @@
 {
   networking.wireguard.enable = true;
 
-  networking.wireguard.interfaces = {
+  networking.wg-quick.interfaces = {
     homelab = {
-      ips = [ "10.100.0.2/32" ];
+      autostart = false;
+      type = "wireguard";
+      address = [ "10.100.0.2/32" ];
       listenPort = 1235;
       mtu = 1360;
       privateKeyFile = config.sops.secrets.wireguard-privatekey.path;
@@ -15,7 +17,6 @@
             "10.1.2.0/24"
             "10.1.1.0/24"
           ];
-          name = "x1";
           endpoint = "casa-vpn.ayoubedd.me:51820";
           persistentKeepalive = 25;
         }
